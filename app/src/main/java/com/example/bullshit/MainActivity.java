bd37336b05bb18feadd2b382d6b72fb7ba8dc901
dgorev002 <denisgorev1992@gmail.com>
2018-12-30 23:31:04 +0300

    ок

--- app/src/main/java/com/example/bullshit/MainActivity.java
+++ app/src/main/java/com/example/bullshit/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.bullshit;
 
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.media.Image;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
@@ -62,8 +63,18 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         textView = (TextView) findViewById(R.id.textView);
+        Button button2 = (Button) findViewById(R.id.button2);
         button1 = (Button) findViewById(R.id.button);
         imageView = (ImageView) findViewById(R.id.imageView1);
+
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, Message.class);
+                startActivity(intent);
+            }
+        });
+
         final String[] words = getResources().getStringArray(R.array.adjectives_m);
         final String[] words2 = getResources().getStringArray(R.array.nouns_m);
         final String[] words3 = getResources().getStringArray(R.array.adjectives_f);
---
+++ app/src/main/java/com/example/bullshit/Message.java
@@ -0,0 +1,34 @@
+package com.example.bullshit;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class Message extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_message);
+
+        Button button = (Button) findViewById(R.id.send);
+
+
+       button.setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View v) {
+               EditText messageView = (EditText)findViewById(R.id.message);
+               String messageText = messageView.getText().toString(); //получение текста из объекта messageView
+               Intent intent = new Intent(Intent.ACTION_SEND); //создание нового интента с опцией отправки сообшений
+               intent.setType("text/plain"); //описание типа данных
+               intent.putExtra(Intent.EXTRA_TEXT, messageText); //включение дополнительной информации в интент (сообщение)
+               String chooserTitle = getString(R.string.chooser); //получение заголовка из Strings.xml для заголовка окна выбора приложения
+               Intent chosenIntent = Intent.createChooser(intent, chooserTitle); //createChooser возвращает объект Intent со всеми заданными свойствами, но с выводом окна выбора активности
+               startActivity(chosenIntent); //запуск активности, вызываемой интентом
+           }
+       });
+    }
+}
